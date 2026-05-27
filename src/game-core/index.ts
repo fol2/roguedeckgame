26a77f73649b1d2ea426d1d122221a7496795355
James To <fol2hk@gmail.com>
2026-05-27 02:22:32 +0100

    refactor(core): share ability target descriptors

--- src/game-core/index.ts
+++ src/game-core/index.ts
@@ -20,6 +20,7 @@ export * from "./data/balance/act1-normal";
 export * from "./data/builders";
 export * from "./data/encounters/forest-encounters";
 export * from "./systems/card-actions";
+export * from "./systems/ability-descriptors";
 export * from "./systems/card-piles";
 export * from "./systems/class-modifiers";
 export * from "./systems/combat";
